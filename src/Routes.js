dbc8fadd3f499f08e83f985fe5e5e8de974dd3e8
quangtran <quetoiohue@gmail.com>
2020-06-19 13:58:02 +0700

    Adding a Netlify build script

--- src/Routes.js
+++ src/Routes.js
@@ -11,6 +11,8 @@ import AuthenticatedRoute from "./components/AuthenticatedRoute";
 import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 
 export default function Routes() {
+  console.log(process.env);
+
   return (
     <Switch>
       <Route exact path="/">
